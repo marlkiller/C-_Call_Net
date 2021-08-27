011b5ff9de7a99b676670ad6b0407cf556003306
voidm <marlkiller@voidm.com>
2021-08-27 12:26:36 +0800

    dev

--- dllmain.cpp
+++ dllmain.cpp
@@ -9,6 +9,9 @@
 #include<cstdio>
 #include <gcroot.h>
 #include "dllmain.h"
+#include<stdio.h>
+#include<psapi.h>
+
 #using "CSDllMain.dll"
 
 using namespace CSDllMain;
@@ -17,12 +20,8 @@ using namespace CSDllMain;
 //gcroot<Class1^> testClass;
 
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                       )
+BOOL APIENTRY DllMain( HMODULE hModule,DWORD  ul_reason_for_call,LPVOID lpReserved)
 {
-
     switch (ul_reason_for_call)
     {
         // DLL_PROCESS_ATTACH: 进程第一次加载时通知
@@ -30,38 +29,39 @@ BOOL APIENTRY DllMain( HMODULE hModule,
             // CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)main, hModule, 0, NULL);
             injection();
             break;
-        // DLL_THREAD_ATTACH: 新线程创建时通知
-        case DLL_THREAD_ATTACH:
-            break;
+            // DLL_THREAD_ATTACH: 新线程创建时通知
+            case DLL_THREAD_ATTACH:
+                break;
 
-        // DLL_THREAD_DETACH: 线程消亡时通知
-        case DLL_THREAD_DETACH:
-            break;
+            // DLL_THREAD_DETACH: 线程消亡时通知
+            case DLL_THREAD_DETACH:
+                break;
 
-        // DLL_PROCESS_DETACH: 进程卸载dll时通知
-        case DLL_PROCESS_DETACH:
-            un_injection();
-            break;
+            // DLL_PROCESS_DETACH: 进程卸载dll时通知
+            case DLL_PROCESS_DETACH:
+                un_injection();
+                break;
     }
     return TRUE;
 }
 
 void main()
 {
-//    MessageBox(NULL, TEXT("C++ DLL Injection Successful"), TEXT("Title"), MB_OK);
-//    testClass = gcnew Class1();
-//    testClass->hook();
+    // MessageBox(NULL, TEXT("C++ DLL Injection Successful"), TEXT("Title"), MB_OK);
+    // testClass = gcnew Class1();
+    // testClass->hook();
 }
 
 void injection()
 {
     MessageBox(NULL, TEXT("C++ DLL Injection Successful"), TEXT("Title"), MB_OK);
-    Class1^ testClass = gcnew Class1();
-    testClass = gcnew Class1();
-    testClass->hook();
+    // Class1^ testClass = gcnew Class1();
+    // testClass = gcnew Class1();
+    // testClass->hook();
 }
 
 void un_injection()
 {
     MessageBox(NULL, TEXT("C++ DLL Un_injection Successful"), TEXT("Title"), MB_OK);
 }
+
--- dllmain.h
+++ dllmain.h
@@ -1,11 +1,34 @@
-#pragma once
+#ifdef DLLMAIN_EXPORTS
+#define DLLMAIN_API __declspec(dllexport)//注意decl前面是两个下划线
+#else
+#define DLLMAIN_API __declspec(dllimport)
+#endif
+
+
+DLLMAIN_API void injection();
+DLLMAIN_API void un_injection();
+DLLMAIN_API void main();
+
+
+// namespace DLLMainSpace
+// {
+
+//导出类
+//class MyDllClass
+// {
+// private:
+//  double a;
+// public:
+//导出的函数
+//DLLMAIN_API MyDllClass();
+//DLLMAIN_API MyDllClass(double a_);
+//DLLMAIN_API double multiply(double b);
+//DLLMAIN_API void display();
+//DLLMAIN_API static void conbine(MyDllClass m1, MyDllClass m2);
+//};
+// }
 
 BOOL DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved);
 
 
-void injection();
-
-void un_injection();
-
-void main();
 
